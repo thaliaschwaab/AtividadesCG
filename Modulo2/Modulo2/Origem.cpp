e08a8d97f22cff15abd3a51552a6b5e30ab5d40a
Thalia Schwaab <thalia.danieli@gmail.com>
2024-06-16 11:33:05 -0300

    update gif

--- Modulo2/Modulo2/Origem.cpp
+++ Modulo2/Modulo2/Origem.cpp
@@ -82,7 +82,7 @@ int main()
 //#endif
 
 	// Criação da janela GLFW
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Modulo2", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "M2: Instanciando objetos na cena 3D - Thalia Schwaab", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Fazendo o registro da função de callback para a janela GLFW
